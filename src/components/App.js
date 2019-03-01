32620d70936a34db1de7c87c8212f58a9c3eefa0
Sean Casey <sean@seanjcasey.com>
2019-03-01 09:59:27 -0400

    added react-router and added basic routing to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import EditablePlacesVisitedMap from '../containers/EditablePlacesVisitedMap';
 
 const App = () => (
-  <div className="app-wrapper">
-    <EditablePlacesVisitedMap />
-  </div>
+  <Router>
+    <div className="app-wrapper">
+      <Route
+        path='/'
+        exact
+        component={EditablePlacesVisitedMap}
+      />
+      <Route
+        path='/travel-maps/:userSlug'
+        component={EditablePlacesVisitedMap}
+      />
+    </div>
+  </Router>
 )
 
 export default App;
