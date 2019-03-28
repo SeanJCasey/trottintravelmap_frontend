969bbcb8143705761b51d08aea22c92d57f89c92
Sean Casey <sean@seanjcasey.com>
2019-03-28 10:12:53 -0400

    added username linked to home to navbar for logged in user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,31 @@ const Navbar = ({ user, onUserLogout }) =>
     <div className="container">
       <Link to={'/'} className="btn btn-default">TripPlot</Link>
       {!user.id &&
-        <Link
-          to={'/login'}
-          className="btn btn-outline-secondary btn-sm"
-        >
-        Sign In
-        </Link>
+        <div className="user-links logged-out">
+          <Link
+            to={'/login'}
+            className="btn btn-outline-secondary btn-sm"
+          >
+          Sign In
+          </Link>
+        </div>
       }
       {user.id &&
-        <button
-          className="btn btn-outline-secondary btn-sm"
-          onClick={onUserLogout}
-        >
-        Logout
-        </button>
+        <div className="user-links logged-in">
+          <div className="account-info">
+            <Link
+              to={'/'}
+            >
+            {user.email}
+            </Link>
+          </div>
+          <button
+            className="btn btn-outline-secondary btn-sm logout"
+            onClick={onUserLogout}
+          >
+          Logout
+          </button>
+        </div>
       }
     </div>
   </div>
